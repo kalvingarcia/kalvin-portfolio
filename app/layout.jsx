ada4b846f71996443bee9890663010208f0b49eb
Kalvin Garcia <plutea.leaf@gmail.com>
2023-11-15 11:39:33 -0600

    added use client and moved metadata exports

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,12 @@
+'use client';
 import * as React from 'react';
 import Script from 'next/script';
 import {createTheme} from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
-import {Inter} from 'next/font/google';
 
 import ThemeRegistry from './__components/theme-registry';
 import {ColorModeContext, ChangeThemeContext} from './__utils/contexts';
 import {useTheme} from './__utils/hooks';
-import './globals.css';
-
-const inter = Inter({subsets: ['latin']});
-
-export const metadata = {
-    title: "Kalvin's Porfolio",
-    description: "Here you'll find all of Kalvin Garcia's work and projects. You can also find Kalvin's resume and contact information!",
-};
 
 export default function RootLayout({ children }) {
     const [mode, setMode] = React.useState('light');
@@ -26,7 +18,10 @@ export default function RootLayout({ children }) {
     const theme = React.useMemo(() => createTheme(getTheme(mode)), [mode, getTheme]);
     return (
         <html lang="en">
-            <body className={inter.className}>
+            <head>
+
+            </head>
+            <body className="portfolio-body">
                 <ColorModeContext.Provider value={colorMode}>
                 <ChangeThemeContext.Provider value={changeTheme}>
                     <ThemeRegistry theme={theme} options={{key: 'mui'}}>
@@ -35,8 +30,8 @@ export default function RootLayout({ children }) {
                     </ThemeRegistry>
                 </ChangeThemeContext.Provider>
                 </ColorModeContext.Provider>
-                <Script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js" />
             </body>
+            <Script src="https://cdn.jsdelivr.net/gh/google/code-prettify@master/loader/run_prettify.js" />
         </html>
     )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+export const metadata = {
+    title: "Kalvin's Porfolio",
+    description: "Here you'll find all of Kalvin Garcia's work and projects. You can also find Kalvin's resume and contact information!",
+};
+
 export default function Home() {
     return (
         <React.Fragment>
