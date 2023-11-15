354bbc0ca0f2d8b419d10a18bdde80e1f62a3b1d
Kalvin Garcia <plutea.leaf@gmail.com>
2023-11-14 19:32:41 -0600

    added structure to projects page

---
+++ app/projects/page.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export default function ProjectLibrary() {
+
+    return (
+        <main className='project-library-page'>
+            <ProjectTimelines />
+            <FeaturedProjects />
+        </main>
+    );
+}
