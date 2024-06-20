d61d06196237bb2142cb15e08141c9455e542342
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-20 13:03:39 -0400

    testing the animator component

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,58 @@
 "use client"
 import { useState } from "react";
-import {tss} from "tss-react";
+import {tss, keyframes} from "tss-react";
+import Animator from "./assets/components/animator";
+
+const fadeInUp = keyframes({
+    "0%": {
+        opacity: 0,
+        transform: "translate(0, 100px) scale(0.75)"
+    },
+    "100%": {
+        opacity: 1,
+        transform: "translate(0, 0)"
+    }
+});
+
+const fadeOutDown = keyframes({
+    "0%": {
+        opacity: 1,
+        transform: "translate(0, 0)"
+    },
+    "100%": {
+        opacity: 0,
+        transform: "translate(0, 100px) scale(0.75)"
+    }
+});
 
 const useStyles = tss.create(({color}) => ({
     home: {
-        color
+        background: color,
+        height: 200,
+        width: 400,
+        position: "fixed",
+        top: "calc((100% - 200px) / 2)",
+        left: "calc((100% - 400px) / 2)",
+        borderRadius: 20
+    },
+    enter: {
+        animation: `${fadeInUp} 300ms ease-in forwards`
+    },
+    leave: {
+        animation: `${fadeOutDown} 300ms ease-in forwards`
     }
 }));
 
 export default function Homepage({}) {
-    const [color, setColor] = useState(true);
-    const {classes, cx} = useStyles({color: color? "green" : "red"});
+    const [show, setShow] = useState(false);
 
+    const {classes, cx} = useStyles({color: "pink"});
     return (
-        <main className={classes.home}>
-            This is test text.
-            <button onClick={() => setColor(!color)}>Change</button>
+        <main>
+            <Animator show={show} enter={classes.enter} leave={classes.leave}>
+                <div className={classes.home} />
+            </Animator>
+            <button onClick={() => setShow(!show)}>Show</button>
         </main>
     );
 }
