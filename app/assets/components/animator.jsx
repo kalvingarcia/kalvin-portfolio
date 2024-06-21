1899dd37f39893eb75521e49c51ba3934db6bb02
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-21 13:57:00 -0400

    added updates to child component when the child changes

--- app/assets/components/animator.jsx
+++ app/assets/components/animator.jsx
@@ -9,13 +9,13 @@ const DEFAULT_ANIMATION_DURATION = 300;
  * A wrapper component that animates the child component using CSS classes. Animate uses “events,”
  * which are triggered by timeout functions.
  * 
- * @props The Animator takes 5 props:
- * *    The `show` prop defines whether the component should be rendered. *Defaults to false.*
- * *    The `enter` and `exit` props define the class names that apply the CSS animations they 
+ * @param props The Animator takes 5 props:
+ *  *   The `show` prop defines whether the component should be rendered. *Defaults to false.*
+ *  *   The `enter` and `exit` props define the class names that apply the CSS animations they 
  *      perform. **This prop is required.**
- * *    The `duration` prop is used to define how long the animation will take in terms of ms. 
+ *  *   The `duration` prop is used to define how long the animation will take in terms of ms. 
  *      *Defaults to 300ms*
- * *    The `children` prop should only have 1 child. **This prop is required.**
+ *  *   The `children` prop should only have 1 child. **This prop is required.**
  * @returns The child React component when `show` is true.
  */
 export default function Animator({show = false, enter, exit, duration = DEFAULT_ANIMATION_DURATION, children}) {
@@ -26,6 +26,9 @@ export default function Animator({show = false, enter, exit, duration = DEFAULT_
 
         const [render, setRender] = useState(false); // Defines whether to render the child or not.
         const [child, setChild] = useState(Children.only(children)); // Assert that the child is an only child.
+        useEffect(() => {
+            setChild(Children.only(children));
+        }, [children]);
         useEffect(() => {
             const baseChild = child; // Saving the child without any special class names.
             // Testing the cases of show and hide (mount and unmount).
@@ -41,8 +44,7 @@ export default function Animator({show = false, enter, exit, duration = DEFAULT_
 
         return render && child;
     } catch(error) {
-        error.message = error.message === ENTER_EXIT_ERROR_MESSAGE? ENTER_EXIT_ERROR_MESSAGE : CHILDREN_ERROR_MESSAGE;
-        console.error(error);
+        console.error(error.message === ENTER_EXIT_ERROR_MESSAGE? ENTER_EXIT_ERROR_MESSAGE : CHILDREN_ERROR_MESSAGE);
         return <></>;
     }
 }
