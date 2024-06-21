ccc1351b60c57bd1b6cfc2be4ffba9c5070af843
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-21 13:57:35 -0400

    testing tss and the themer

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 "use client"
-import { useState } from "react";
-import {tss, keyframes} from "tss-react";
+import { useContext, useState} from "react";
+import {keyframes} from "tss-react";
+import {tss, DarkModeContext, ThemeContext} from './assets/components/themer';
 import Animator from "./assets/components/animator";
 
 const fadeInUp = keyframes({
@@ -25,34 +26,41 @@ const fadeOutDown = keyframes({
     }
 });
 
-const useStyles = tss.create(({color}) => ({
+const useStyles = tss.create(({theme}) => ({
     home: {
-        background: color,
-        height: 200,
+        background: theme.neutral.surface.hex(),
+        [`@media (max-width: ${500}px)`]: {
+            width: 200,
+            left: "calc((100% - 200px) / 2)",
+        },
         width: 400,
+        height: 200,
         position: "fixed",
         top: "calc((100% - 200px) / 2)",
         left: "calc((100% - 400px) / 2)",
-        borderRadius: 20
+        borderRadius: 20,
+        transition: "background-color 300ms ease, width 300ms ease, left 300ms ease"
     },
     enter: {
         animation: `${fadeInUp} 300ms ease-in forwards`
     },
-    leave: {
+    exit: {
         animation: `${fadeOutDown} 300ms ease-in forwards`
     }
 }));
 
 export default function Homepage({}) {
     const [show, setShow] = useState(false);
+    const {darkMode, toggleDarkMode} = useContext(DarkModeContext);
 
-    const {classes, cx} = useStyles({color: "pink"});
+    const {classes, cx} = useStyles();
     return (
         <main>
-            <Animator show={show} enter={classes.enter} leave={classes.leave}>
+            <Animator show={show} enter={classes.enter} exit={classes.exit}>
                 <div className={classes.home} />
             </Animator>
             <button onClick={() => setShow(!show)}>Show</button>
+            <button onClick={() => toggleDarkMode()}>Dark Mode</button>
         </main>
     );
 }
