0c103db655965bb0229b04f4675fb140ec4e0a91
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-25 16:44:50 -0400

    testing modal

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,7 +1,20 @@
+"use client"
+import {useState} from "react";
+import Modal from "../assets/components/modal";
+import Button from "../assets/components/button";
+import {Label, Title} from "../assets/components/typography";
+
 export default function Projects({}) {
+    const [open, setOpen] = useState(false);
+
     return (
         <main>
-            
+            <Button onClick={() => setOpen(true)}>
+                <Label>Open</Label>
+            </Button>
+            <Modal open={open} setOpen={setOpen}>
+                <Title>Modal</Title>
+            </Modal>
         </main>
     )
 }
