b97d159b8d0c015d4c1b2f6656cc2761e03b3e26
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-25 14:13:36 -0400

    moved icons into icon button file because of build issues since next uses icon as a reserved filename

--- app/assets/components/icon-button.jsx
+++ app/assets/components/icon-button.jsx
@@ -1,12 +1,17 @@
 "use client"
 import {useCallback} from 'react';
 import {tss} from './themer';
-import Icon from './icon';
 import {useContainerContext, ContainerContextProvider} from '../helper/container';
 import useRippleEffect from '../hooks/ripple';
 
 // IconButton styles.
-const useStyles = tss.create(({theme, role, appearance, containerRole, rippleClass}) => ({
+const useStyles = tss.create(({theme, role, type, appearance, containerRole, rippleClass, isInButton}) => ({
+    icon: {
+        display: "block",
+        position: "relative",
+        fontSize: isInButton? 20 : 30,
+        color: theme[role][`on${type[0].toUpperCase() + type.slice(1)}`].hex()
+    },
     iconButton: {
         outline: "none",
         border: appearance === "outlined"? `1pt solid ${theme[role].onContainer.hex()}` : "none",
@@ -45,6 +50,28 @@ const useStyles = tss.create(({theme, role, appearance, containerRole, rippleCla
         }
     }
 }));
+
+/**
+ * The Icon component is used to display icons from icon fonts.
+ *
+ * @param props This component takes 3 props:
+ *  *   The `className` prop is used to pass classNames to add styles to the Icon.
+ *  *   The `icon` prop gives the icon ligature text that displays the icon from
+ *      the font.
+ *  *   The `iconClass` prop allows users to define their own icon font css class
+ *      to change the icon options available. *Defaults to material-icons.*
+ *
+ * The component also passes forward any other props the user specifies.
+ *
+ * @returns A styled `i` jsx element.
+ */
+export function Icon({className, icon, iconClass = "material-icons", __isInButton = false, ...props}) {
+    const {role, type} = useContainerContext();
+    const {cx, classes} = useStyles({role, type, isInButton: __isInButton});
+    return (
+        <i className={cx(classes.icon, iconClass, className?? "")} {...props}>{icon}</i>
+    );
+}
 
 /**
  * The IconButton is used to make buttons that only have icons. The reason there is
