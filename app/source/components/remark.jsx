da6def520b7237366d4757a1e26b02a44895af80
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-27 14:25:35 -0400

    Reduced the headings down 1

--- app/source/components/remark.jsx
+++ app/source/components/remark.jsx
@@ -1,23 +1,20 @@
-import Markdown from "react-markdown";
-import {Display, Title, Subtitle, Heading, Subheading, Body, Label} from "./typography";
 import Image from "next/image";
+import Markdown from "react-markdown";
+import {Title, Subtitle, Heading, Subheading, Body, Label} from "./typography";
 
 export default function Remark({children}) {
     return (
         <Markdown components={{
             h1({children}) {
-                return <Display>{children}</Display>;
-            },
-            h2({children}) {
                 return <Title>{children}</Title>;
             },
-            h3({children}) {
+            h2({children}) {
                 return <Subtitle>{children}</Subtitle>;
             },
-            h4({children}) {
+            h3({children}) {
                 return <Heading>{children}</Heading>;
             },
-            h5({children}) {
+            h4({children}) {
                 return <Subheading>{children}</Subheading>;
             },
             p({children}) {
