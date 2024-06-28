1320f05eae7e1ffeb25010d328bc987882d5fe29
Kalvin Garcia <kalvigarcia@gmail.com>
2024-06-28 15:54:21 -0400

    styled remark

--- app/source/components/remark.jsx
+++ app/source/components/remark.jsx
@@ -1,34 +1,85 @@
-import Image from "next/image";
 import Markdown from "react-markdown";
 import {Title, Subtitle, Heading, Subheading, Body, Label} from "./typography";
 import Code from "./code";
+import {tss} from "./themer";
+import Button from "./button";
+
+const useStyles = tss.create(({theme}) => ({
+    title: {
+        marginBottom: 40,
+        color: theme.primary.accent.hex()
+    },
+    subtitle: {
+        marginBottom: 20,
+        color: theme.tertiary.accent.hex()
+    },
+    heading: {
+        marginBottom: 10,
+        color: theme.primary.onContainer.hex()
+    },
+    subheading: {
+        marginBottom: 10,
+        color: theme.tertiary.onContainer.hex()
+    },
+    button: {
+        display: "inline-flex"
+    },
+    body: {
+        marginBottom: 10,
+        opacity: 0.75
+    },
+    image: {
+        marginBottom: 10,
+        width: "100%",
+        height: "100%",
+        "& img": {
+            objectPosition: "center",
+            objectFit: "contain"
+        }
+    },
+    code: {
+        marginBottom: 10
+    }
+}));
 
 export default function Remark({children}) {
+    const {classes} = useStyles();
     return (
         <Markdown components={{
             h1({children}) {
-                return <Title>{children}</Title>;
+                return <Title className={classes.title}>{children}</Title>;
             },
             h2({children}) {
-                return <Subtitle>{children}</Subtitle>;
+                return <Subtitle className={classes.subtitle}>{children}</Subtitle>;
             },
             h3({children}) {
-                return <Heading>{children}</Heading>;
+                return <Heading className={classes.heading}>{children}</Heading>;
             },
             h4({children}) {
-                return <Subheading>{children}</Subheading>;
+                return <Subheading className={classes.subheading}>{children}</Subheading>;
+            },
+            h5({children}) {
+                const params = children.split(',');
+                return (
+                    <Button className={classes.button} appearance="outlined" onClick={() => setTimeout(() => window.open(params[1], "_blank"), 300)}>
+                        <Label>{params[0]}</Label>
+                    </Button>
+                );
             },
             p({children}) {
-                return <Body>{children}</Body>;
-            },
-            code({children}) {
-                return ""
+                return <Body className={classes.body}>{children}</Body>;
             },
             img({src, alt}) {
-                return src? <Image src={src} alt={alt} width={200} height={200} /> : "";
+                return (src?
+                    <div className={classes.image}>
+                        <img src={src} alt={alt} /> 
+                    </div>
+                    : 
+                    ""
+                );
             },
             code({className, children}) {
-                return <Code language={className.replace("language-", "")}>{children}</Code>;
+                return <Code className={classes.code} language={className.replace("language-", "")}>{children}</Code>;
             },
             pre({children}) {
                 return children;
