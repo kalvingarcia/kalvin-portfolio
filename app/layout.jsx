2a4fc587cf75836d4d8782e6f1a66a9e00830d5c
Kalvin Garcia <kalvigarcia@gmail.com>
2024-07-01 11:41:56 -0400

    made layout only render body when on client to fix hydration issues

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
-//import {cookies} from 'next/headers';
+"use client"
 import Local from "next/font/local";
 import {getCookie} from "cookies-next";
+import {useEffect, useState} from "react";
 import {GlobalStyles} from "tss-react";
 import Themer from "./source/components/themer";
 import PalettePicker from './content/palette-picker';
@@ -16,7 +17,12 @@ const kalvinIconsFont = Local({
 });
 
 export default function Layout({children}) {
-    const kalvinIcons = {
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    const prerenderDefaults = {
         ".kalvin-icons": {
             fontFamily: "var(--kalvin-icons)",
             fontWeight: "normal",
@@ -36,13 +42,17 @@ export default function Layout({children}) {
         }
     }
     return (
-            <html lang="en">
-                <body className={kalvinIconsFont.variable}>
-                    <GlobalStyles styles={kalvinIcons} />
-                    <Themer darkModeDefault={(/true/i).test(getCookie("kalvinPortfolioDarkMode"))} themeDefault={getCookie("kalvinPortfolioTheme")} palettePresets={customPalettes}>
-                        <PalettePicker />
-                        {children}
-                    </Themer>
+            <html lang="en" className={kalvinIconsFont.variable}>
+                <body>
+                    <GlobalStyles styles={prerenderDefaults} />
+                    {isClient?
+                        <Themer darkModeDefault={(/true/i).test(getCookie("kalvinPortfolioDarkMode"))} themeDefault={getCookie("kalvinPortfolioTheme")} palettePresets={customPalettes}>
+                            <PalettePicker />
+                            {children}
+                        </Themer>
+                        :
+                        ""
+                    }
                 </body>
             </html>
     );
