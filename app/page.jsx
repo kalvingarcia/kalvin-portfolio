d2a3d415c2560a25af0c4272bb96e1ce228ce1e1
Kalvin Garcia <kalvigarcia@gmail.com>
2024-07-02 12:57:28 -0400

    added a slide scroll effect that I still need to play with

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,37 @@
 "use client"
-import {useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {tss} from './source/components/themer';
 import Splash from "./content/splash";
 import Button from "./source/components/button";
+import useSlideScroll from "./content/slide-scroll";
 
 const useStyles = tss.create(({theme}) => ({
-    homepage: {
-
+    slideScroll: {
+        scrollbarWidth: "none",
+        "&::-webkit-scrollbar": {
+            display: "none"
+        }
     }
 }));
 
-export default function Homepage({}) {
-    const [show, setShow] = useState(true);
+const SLIDE_COUNT = 3;
 
+export default function Homepage({}) {
+    const [show, setShow] = useState(false);
     const {classes} = useStyles();
+
+    useSlideScroll(SLIDE_COUNT);
+    useEffect(() => {
+        document.body.classList.add(classes.slideScroll);
+        document.documentElement.classList.add(classes.slideScroll);
+    }, []);
+
     return (
         <section className={classes.homepage}>
             <Splash show={show} setShow={setShow} />
-            <Button onClick={() => window.location.href = "/projects"} />
+            <div style={{width: "100vw", height: "100vh"}} />
+            <div style={{width: "100vw", height: "100vh"}} />
+            <div style={{width: "100vw", height: "100vh"}} />
         </section>
     );
 }
