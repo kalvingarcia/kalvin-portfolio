da9d2259c298c10df5de6113e140e7a6d3a7f728
Kalvin Garcia <kalvigarcia@gmail.com>
2024-07-11 12:05:41 -0400

    added fetch to project slide

--- app/content/project.jsx
+++ app/content/project.jsx
@@ -1,29 +1,64 @@
+import {useState, useEffect} from "react";
+import Button from "../source/components/button";
 import {tss} from "../source/components/themer";
+import { Label, Subtitle } from "../source/components/typography";
+import ProjectCard from "./project-card";
+import { Icon } from "../source/components/icon-button";
 
 const useStyles = tss.create(({theme}) => ({
     slide: {
         height: "100vh",
-        width: "100vw",
-        display: "flex",
+        width: "100%",
         overflow: "hidden",
+        display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        gap: 50,
+        flexDirection: "column",
+    },
+    projects: {
+        width: "100%",
+        padding: 20,
+        display: "flex",
+        gap: 20,
+        overflow: "hidden",
+        overflowX: "auto",
+        "& > *": {
+            flex: "0 0 400px",
+        },
         [`@media (max-width: ${1280}px)`]: {
-            height: "100vh",
-            width: "100vw",
-            top: 500,
-            left: 0,
-            flexDirection: "column-reverse",
+            flexDirection: "column",
+            "& > *": {
+                flex: "1 1 200px",
+            },
         }
     }
 }));
 
 export default function Project({}) {
+    const [featured, setFeatured] = useState({});
+    const [isClient, setIsClient] = useState(false);
+    useEffect(() => {
+        (async () => {
+            setFeatured(await fetch("./featured.json").then(response => response.json()));
+            setIsClient(true);
+        })();
+    }, []);
+
     const {classes} = useStyles();
     return (
         <div className={classes.slide}>
-
+            <Subtitle>Featured Projects</Subtitle>
+            <div className={classes.projects}>{isClient?
+                featured.map(({directory, name, description}) => (
+                    <ProjectCard image={`/images/${directory}/wireframes.png`} heading={name} body={description} directory={directory} />
+                ))
+                :
+                ""
+            }</div>
+            <Button appearance="outlined" onClick={() => setTimeout(() => window.location.href = "https://projects.kalvingarcia.com/", 300)}>
+                <Label>Want to see more?</Label>
+                <Icon icon="arrow_outward" />
+            </Button>
         </div>
     );
 }
