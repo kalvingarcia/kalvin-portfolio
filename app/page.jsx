81b5f2b7e80e2cfa1ef5db6955af33b2827d3df1
Kalvin Garcia <kalvigarcia@gmail.com>
2024-07-12 12:04:58 -0400

    moved content into a separate div so that the scroll bar displays all the way on the right

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,26 @@
 "use client"
-import {useCallback, useEffect, useState} from "react";
+import {useState} from "react";
 import {tss} from './source/components/themer';
-import Splash from "./content/splash";
-import Button from "./source/components/button";
-import Slider from "./source/components/slider";
-import Bio from "./content/bio";
-import Project from "./content/project";
 import Overlay from "./content/overlay";
 import PalettePicker from "./content/palette-picker";
+import Bio from "./content/bio";
+import Project from "./content/project";
+import Contact from "./content/contact";
+import Splash from "./content/splash";
 
 const useStyles = tss.create(({theme}) => ({
     homepage: {
         margin: "auto",
         width: "100%",
-        maxWidth: 1280,
         maxHeight: "100vh",
         overflowX: "hidden",
         overflowY: "auto"
+    },
+    content: {
+        margin: "auto",
+        width: "100%",
+        maxWidth: 1280,
+        maxHeight: "100%"
     }
 }));
 
@@ -29,9 +33,12 @@ export default function Homepage({}) {
             <Overlay />
             <PalettePicker />
             <section className={classes.homepage}>
+                <div className={classes.content}>
+                    <Bio />
+                    <Project />
+                    <Contact />
+                </div>
                 <Splash show={show} setShow={setShow} />
-                <Bio />
-                <Project />
             </section>
         </>
     );
