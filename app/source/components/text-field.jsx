e654fb7d7361cc42496d0ba15f2c02b0ef2f5ee6
Kalvin Garcia <kalvigarcia@gmail.com>
2025-02-19 16:10:46 -0500

    updated text-fields to be more usable

--- app/source/components/text-field.jsx
+++ app/source/components/text-field.jsx
@@ -1,47 +1,45 @@
 import {useCallback, useEffect, useState} from "react";
-import useRippleEffect from "../hooks/ripple";
 import {tss} from "./themer";
 import {Label} from "./typography";
 
-const useStyles = tss.create(({theme, filled, rippleClass}) => ({
+const useStyles = tss.create(({theme, filled, error}) => ({
     container: {
         width: "100%",
         height: "100%",
         display: "flex",
         flexDirection: "column",
         "& .helper": {
-            padding: 5,
+            padding: "5px",
             width: "100%",
             height: "fit-content",
             fontSize: "0.75rem",
-            color: theme.neutral.onContainer.alpha(0.5).hexa(),
-            tansition: "color 200ms ease-in-out"
+            color: error? theme.onError : theme.neutral.onContainer.alpha(0.5).hexa(),
+            transition: "color 200ms ease-in-out"
         },
         "&:focus-within .helper": {
-            color: theme.tertiary.accent.hex()
-        },
+            color: error? theme.onError : theme.tertiary.accent.hex()
+        }
     },
     textField: {
         width: "100%",
-        height: "3rem",
+        height: "55px",
         position: "relative",
         cursor: "text",
         borderRadius: "10px 10px 0 0",
-        backgroundColor: theme.neutral.containerHighest.hex(),
+        backgroundColor: error? theme.error + "3F" : theme.neutral.containerHighest.hex(),
         padding: "10px 20px",
         overflow: "hidden",
-        clipPath: "inset(0 0 0 0 round 10px 10px 0 0)",
-        borderBottom: `1pt solid ${theme.neutral.onContainer.alpha(0.5).hexa()}`,
-        transition: "border-bottom 200ms ease-in-out",
+        borderBottom: `1pt solid ${error? theme.onError + "7F" : theme.neutral.onContainer.alpha(0.5).hexa()}`,
+        transition: "background-color 300ms ease, border-bottom 200ms ease-in-out",
         "&:focus-within": {
-            borderColor: theme.tertiary.accent.hex()
+            borderColor: error? theme.onError : theme.onSecondary
         },
         "&::before": {
             content: "''",
             position: "absolute",
             inset: 0,
             opacity: 0,
-            backgroundColor: theme.neutral.onContainer.hex()
+            backgroundColor: error? theme.onError : theme.neutral.onContainer.hex()
         },
         "&:hover::before": {
             opacity: 0.2
@@ -56,114 +54,94 @@ const useStyles = tss.create(({theme, filled, rippleClass}) => ({
             transformOrigin: "bottom",
             transform: "scale(0)",
             opacity: 0,
-            transition: "transform 200ms ease, opacity 200ms ease",
-            borderBottom: `1pt solid ${theme.tertiary.accent.hex()}`
+            transition: "transform 100ms ease, opacity 200ms ease",
+            borderBottom: `1pt solid ${error? theme.onError : theme.tertiary.accent.hex()}`
         },
         "&:focus-within::after": {
             transform: "scale(1)",
             opacity: 1,
         },
         "& .field": {
+            pointerEvents: "auto",
+            zIndex: 1,
             width: "100%",
             outline: "none",
             border: "none",
             position: "relative",
-            bottom: -10,
+            bottom: "-15px",
             backgroundColor: "transparent",
+            color: theme.body,
             fontFamily: "var(--body-font)",
-            color: theme.neutral.onContainer.hex(),
             "&::placeholder": {
-                color: "transparent",
-                fontFamily: "var(--body-font)"
+                color: "transparent"
             },
             "&:focus::placeholder": {
-                color: theme.neutral.onContainer.alpha(0.5).hexa()
-            },
-            "&:autofill": {
-                outline: "none",
-                border: "none",
-                transition: "background-color 600000s 0s, color 600000s 0s, font 600000s 0s",
-                fontFamily: "var(--body-font)",
+                color: error? theme.onError + "7F" : theme.body + "7F"
             }
         },
         "& .label": {
-            top: 25,
+            top: "30px",
             position: "absolute",
             transformOrigin: "top left",
-            transform: filled? "translate(0, -100%) scale(0.8)" : "translate(0, -50%)",
+            transform: filled? "translate(0, -80%) scale(0.7)" : "translate(0, -50%)",
             opacity: 0.5,
             transition: "transform 200ms ease, opacity 200ms ease, color 200ms ease",
-            color: theme.neutral.onContainer.hex()
+            color: error? theme.onError : theme.neutral.onContainer.hex(),
+            fontWeight: "bold"
         },
         "&:focus-within .label": {
-            transform: "translate(0, -100%) scale(0.8)",
-            fontWeight: "bold",
+            transform: "translate(0, -80%) scale(0.7)",
             opacity: 1,
-            color: theme.tertiary.accent.hex()
+            color: error? theme.onError : theme.tertiary.accent.hex()
         },
         "&:has(input:required) .label::after": {
             content: "'*'"
-        },
-        [`& .${rippleClass}`]: {
-            backgroundColor: theme.neutral.onContainer.hex()
         }
-    },
-    textArea: {
-        width: "100%",
-        resize: "none"
     }
 }));
 
-export default function TextField({className, label, sampleText, helperText, onChange, ...props}) {
-    const {rippleClass, rippleExpand, rippleFade} = useRippleEffect();
+export default function TextField({className, label, helperText, value, onChange, onClick, error, ...props}) {
+    const [textField, setTextField] = useState(undefined);
+    const handleContainerClick = event => {
+        if(textField)
+            textField.focus();
+        onClick?.(event);
+    }
 
-    const handleClick = useCallback(() => {
-        const textField = document.getElementById(label);
-        textField.focus();
-        textField.setSelectionRange(0, textField.value.length);
-    }, []);
-    const handleBlur = useCallback(event => {
-        if(event.relatedTarget?.contains(event.currentTarget))
-            event.currentTarget.focus();
-    }, []);
-
-    const [filled, setFilled] = useState(false);
-    const handleFill = useCallback(event => {
+    const [text, setText] = useState(value?? "");
+    useEffect(() => {
+        setText(value?? "");
+        setFilled(!value || value === ""? false : true);
+    }, [value]);
+    const [filled, setFilled] = useState(!value || value === ""? false : true);
+    const handleFill = event => {
         const target = event.currentTarget;
         setFilled(true);
         if(!target.value || !target.value.trim().length)
             setFilled(false);
-        onChange?.(event);
-    }, []);
-    useEffect(() => {
-        document.getElementById(label).addEventListener("input", handleFill);
-        return () => document.getElementById(label).removeEventListener("input", handleFill);
-    }, [])
+        setText(target.value);
+        onChange?.(target.value);
+    }
 
-    const {cx, classes} = useStyles({filled, rippleClass});
+    const {cx, classes} = useStyles({filled, error});
     return (
         <div className={classes.container}>
-            <div
-                tabIndex={-1} // Negative tabIndex to give the div focusability but keep it out of focus order.
-                className={cx(classes.textField, className)}
-                onMouseDown={filled? undefined : rippleExpand}
-                onMouseUp={rippleFade}
-                onClick={handleClick}
-            >
+            <div tabIndex={-1} className={cx(classes.textField, className)} onClick={handleContainerClick}>
                 <label htmlFor={label}>
                     <Label className="label">{label}</Label>
-                    {sampleText? <Label className="sample">{sampleText}</Label> : ""}
-                    <input 
+                    <input
+                        ref={element => setTextField(element)}
                         className="field" 
                         id={label}
                         name={label}
-                        type="text" 
-                        onBlur={handleBlur}
+                        type="text"
+                        value={text}
+                        onChange={handleFill}
                         {...props}
                     />
                 </label>
             </div>
-            {helperText? <Label className="helper">{helperText}</Label> : ""}
+            {helperText && <Label className="helper">{helperText}</Label>}
         </div>
     )
 }
