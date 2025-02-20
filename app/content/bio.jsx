2bad88949286168715ff105043058b082dc59543
Kalvin Garcia <kalvigarcia@gmail.com>
2025-02-20 15:06:17 -0500

    edited the bio

--- app/content/bio.jsx
+++ app/content/bio.jsx
@@ -1,6 +1,4 @@
 import {tss} from "../source/components/themer";
-import Slider from "../source/components/slider";
-import {useCallback, useState} from "react";
 import {Body, Title, Label} from "../source/components/typography";
 import {Icon} from "../source/components/icon-button";
 import Button from "../source/components/button";
@@ -40,15 +38,6 @@ const useStyles = tss.create(({theme}) => ({
     }
 }));
 
-const DEFAULT_BIO = 2;
-const bioList = [
-    "I'm a human that likes to cook and code.",
-    "I’m a Frontend Developer dedicated to creative problem-solving and innovative thinking.",
-    "I am a Frontend Developer with expertise in design, React, Next, and AI development. I am dedicated to creative problem-solving and innovative thinking.",
-    "I am a Frontend Developer skilled in React, Next, and AI development. I bring a diverse skill set including mobile and web design, front-end development, machine learning, and natural language processing. My projects, like Urban Archaeology's Website and Comensal, reflect my dedication to creative problem-solving and innovative thinking.",
-    
-];
-
 export default function Bio({show}) {
     const {fadeInactive, fadeIn, fadeActive} = useFadeAnimation();
     const {classes} = useStyles();
@@ -60,13 +49,11 @@ export default function Bio({show}) {
                     <Title className={classes.headline}>About me</Title>
                 </div>
                 <Body className={classes.bio}>
-                    I am a Frontend Developer with extensive experience in React, Next, and AI development. I am always searching
-                    for opportunities to apply my diverse skill set, which includes web design, front-end development, machine learning, 
-                    and natural language processing. My projects, like Urban Archaeology's Website and Comensal, reflect my dedication
-                    to creative problem-solving and innovative thinking. When I'm not coding, I'm fantasizing about how to make my office
-                    space cooler. I enjoy long walks in the park with my partner, where we discuss the profound mysteries of life (the
-                    plot lines of whatever show we're watching). Also, I firmly believe all offices should have emotional support cats,
-                    and I'm willing to die on that hill.
+                    I'm a Software Engineer driven by curiosity and a passion for continuous learning.
+                    I thrive on tackling new challenges, developing innovative projects, and exploring
+                    creative ideas. Whether it's coding, refining application concepts, or experimenting
+                    with new recipes, I'm always building something. I also believe every office should
+                    have an emotional support cat—and I'm prepared to die on that hill.
                 </Body>
                 <div className={classes.buttons}>
                     <Button className={classes.projectsButton} role="primary" appearance="filled" onClick={() => setTimeout(() => window.location.href = "https://projects.kalvingarcia.com/", 300)}>
